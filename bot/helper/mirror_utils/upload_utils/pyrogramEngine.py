40d12b5d2696b3fece40494ddc7052815c042a47
PachGit <108862443+PachGit@users.noreply.github.com>
2022-08-07 14:51:49 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -69,13 +69,13 @@ class TgUploader:
 
     def __upload_file(self, up_path, file_, dirpath):
         if CUSTOM_FILENAME is not None:
-            cap_mono = f"{CUSTOM_FILENAME} <b>{file_}</b>\n\n<b>@CB_PSARIPS1</b>"
+            cap_mono = f"{CUSTOM_FILENAME} <b>{file_}</b>\n\n<b><a href=https://t.me/CBMainChannel>📀 ᴄɪɴᴇᴍᴀ ʙᴇᴀᴄᴏɴ 📀</a></b>"
             file_ = f"{CUSTOM_FILENAME} {file_}"
             new_path = ospath.join(dirpath, file_)
             osrename(up_path, new_path)
             up_path = new_path
         else:
-            cap_mono = f"<b>{file_}</b>\n\n<b>@CB_PSARIPS1</b>"
+            cap_mono = f"<b>{file_}</b>\n\n<b><a href=https://t.me/CBMainChannel>📀 ᴄɪɴᴇᴍᴀ ʙᴇᴀᴄᴏɴ 📀</a></b>"
         notMedia = False
         thumb = self.__thumb
         self.__is_corrupted = False
