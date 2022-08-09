fd6dd69b4675aeec50c097e4f2a76b02c51b4aeb
tgxthings <110894299+tgxthings@users.noreply.github.com>
2022-08-09 16:49:27 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -57,8 +57,7 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Owner", "https://t.me/EnthadaNokunne")
-    buttons.buildbutton("Friend", "tg://settings/")
+    buttons.buildbutton("Owner", "tg://settings/")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
